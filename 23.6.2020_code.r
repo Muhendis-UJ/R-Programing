8bf8f9f04bd06e72499cc1584b50e36ea2762b5d
Umer Junjoa <umerjunjoa@gmail.com>
2020-06-23 22:30:23 +0500

    new lines added

--- 23.6.2020_code.r
+++ 23.6.2020_code.r
@@ -1,2 +1,17 @@
 date = "23.06.2020"
 R_code = "this is new code file"
+
+#Vectorized operation practice
+  x <- 1:6; y <- 6:9
+  x > 2
+  x >= 2
+  x * y
+  x/y
+  
+  #Vectorized matrix operations
+  x <- matrix(1:4,2,2); y<- matrix(4:7,2,2)
+  x * y #element wise multiplication
+  x / y #element wise division
+  x %*% y #matrix dot product
+  
+#Date & Time
