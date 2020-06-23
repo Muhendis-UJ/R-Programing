4c6f64642db3f96ca8be933eb4900ed7325b0542
Umer Junjoa <umerjunjoa@gmail.com>
2020-06-23 23:14:13 +0500

    new lines addedd

--- 23.6.2020_code.r
+++ 23.6.2020_code.r
@@ -15,10 +15,20 @@ R_code = "this is new code file"
   x %*% y #matrix dot product
   
 #Date & Time
+  as.Date("2020-6-23") #converting string to date("YYYY-MM-DD") format
   x <- Sys.time() #to check current system time
   class(x)    #this shows it x has two classes "POSIXct", "POSIXlt"
   p <- as.POSIXlt(x) #converting to POSIXlt which has many attributes
   names(unclass(p)) #show all attributes like sec,min,hour etc
   p$sec #shows seconds
   p$zone #show time zone
+  #converting date in time format
+  datestring <- c("march 13 1995, 07:15", "September 15 1995, 09:00")
+  x <- strptime(datestring, "%B %d %Y, %H:%M") #B is month name
+  
+  
+  
+  
+  
+  
   
